3cb0ce8577f46737caeb39cf3e5fe78fdd5dc915
xaav <xaav@xaav.tk>
2011-06-10 14:49:04 -0500

    Renamed view action.

--- lib/src/Xaav/WikiBundle/Controller/DefaultController.php
+++ lib/src/Xaav/WikiBundle/Controller/DefaultController.php
@@ -18,7 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function viewAction($page_name = 'index')
     {
         ob_start();
 
