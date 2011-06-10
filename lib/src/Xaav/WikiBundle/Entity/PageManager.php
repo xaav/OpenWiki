f2188412ca0e28748a3232639cd1f21ab1c24d09
xaav <xaav@xaav.tk>
2011-06-10 14:58:51 -0500

    Updated page manager.

--- lib/src/Xaav/WikiBundle/Entity/PageManager.php
+++ lib/src/Xaav/WikiBundle/Entity/PageManager.php
@@ -4,13 +4,33 @@ namespace Xaav\WikiBundle\Entity;
 
 class PageManager
 {
-    public function findByName($name)
+    protected $pages_directory;
+
+    public function __construct($pages_directory)
     {
-        //Get a page by name
+        $this->pages_directory = constant($pages_directory); //Hack
+    }
+
+    public function findByTitle($title)
+    {
+        $page = new Page();
+        $page->setTitle($title);
+
+        $page->setContent(file_get_contents($this->getFilenameByTitle($title)));
+
+        return $page;
     }
 
     public function persist(Page $page)
     {
-        //Save the page
+        $filename = $this->getFilenameByTitle($page->getTitle());
+        $contents = $page->getContent();
+
+        file_put_contents($filename, $contents);
+    }
+
+    protected function getFilenameByTitle($title)
+    {
+        return $this->pages_directory.'/'.$title.'.markdown';
     }
 }
