374aae82fc02ed5672472aaee9ce6eab1e60ccc9
xaav <xaav@xaav.tk>
2011-06-11 22:26:15 -0500

    Moved configs to bundle-specific.

---
+++ lib/src/Xaav/WikiBundle/DependencyInjection/XaavWikiExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Xaav\WikiBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class XaavWikiExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
