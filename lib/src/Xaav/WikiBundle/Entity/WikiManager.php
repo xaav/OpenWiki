b120a6522f93f394008befef4c7a7764b389ff77
xaav <xaav@xaav.tk>
2011-06-13 10:05:44 -0500

    Removed deprecated methods.

--- lib/src/Xaav/WikiBundle/Entity/WikiManager.php
+++ lib/src/Xaav/WikiBundle/Entity/WikiManager.php
@@ -9,10 +9,7 @@ use Glip\Git;
 
 class WikiManager
 {
-    /**
-     * @deprecated
-     */
-    protected $data_directory;
+
     protected $page_repository;
     protected $version_repository;
 
@@ -21,37 +18,9 @@ class WikiManager
      */
     protected $git_repository;
 
-    public function __construct($data_directory, Git $git_repository)
+    public function __construct(Git $git_repository)
     {
         $this->git_repository = $git_repository;
-        $this->data_directory = $data_directory;
-    }
-
-    /**
-     * @return GitBranch
-     * @deprecated
-     */
-    public function getMaster()
-    {
-        return $this->git_repository['master'];
-    }
-
-    /**
-     * @return GitCommit
-     * @deprecated
-     */
-    public function getTip()
-    {
-        return $this->getMaster()->getTip();
-    }
-
-    /**
-     * @return GitTree
-     * @deprecated
-     */
-    public function getTree()
-    {
-        return $this->getTip()->tree;
     }
 
     public function getPageRepository()
@@ -62,7 +31,7 @@ class WikiManager
         }
         else {
 
-            return $this->page_repository = new PageRepository($this->data_directory, $this);
+            return $this->page_repository = new PageRepository($this->git_repository);
         }
     }
 
